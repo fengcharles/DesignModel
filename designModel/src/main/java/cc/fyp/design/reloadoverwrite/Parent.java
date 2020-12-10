e3f10d4c95923bc655abcc9fa041bec8fdf42273
von <fengyinpeng@91top.com>
2020-12-10 15:45:39 +0800

    项目转移

---
+++ designModel/src/main/java/cc/fyp/design/reloadoverwrite/Parent.java
@@ -0,0 +1,20 @@
+package cc.fyp.design.reloadoverwrite;
+
+import java.io.IOException;
+
+/**
+ * @author von
+ * @description:
+ * @date 2019-06-0720:33
+ */
+public class Parent {
+
+    protected  Integer test(Integer x) throws Exception {
+        return 1;
+    }
+
+    public  final Double test1(){
+        return Double.MAX_VALUE;
+    }
+
+}
